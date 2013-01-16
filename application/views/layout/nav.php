d19670f2053df8fcc787d6ac7ccf88bcf2bb36a4
Anton Kjörck Lindén <anton@noviumdesign.se>
2013-01-16 14:23:11 +0100

    Updated nav and a quick config

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -2,12 +2,11 @@
 	<ul id="nav">
 		<li class="i_house"><a href="/"><span>Dashboard</span></a></li>
 		<li class="i_create_write"><a href="/orders/view"><span>Orders<i class="fr"><?= $numOrders[0][0]; ?></i></span></a></li>
-		<li class="i_create_write"><a href="/products/view"><span>Products<i class="fr"><?= $numProducts[0][0]; ?></i></span></a></li>
+		<li class="i_shopping_cart"><a href="/products/view"><span>Products<i class="fr"><?= $numProducts[0][0]; ?></i></span></a></li>
 		<li class="i_user"><a href="/customers/view"><span>Customers<i class="fr"><?= $numCustomers[0][0]; ?></i></span></a></li>
-		<li class="i_truck"><a href="/invoices/view"><span>Invoices<i class="fr"><?= $numInvoices[0][0]; ?></i></span></a></li>
+		<li class="i_cash_register"><a href="/invoices/view"><span>Invoices<i class="fr"><?= $numInvoices[0][0]; ?></i></span></a></li>
 		<!--
 		<li class="i_folder_love"><a href="/?nav=crm"><span>CRM</span></a></li>
-		<li class="i_cash_register"><a href="/?nav=invoice"><span>Invoices</span></a></li>
 		<li class="i_question"><a href="/?nav=quote"><span>Quotes</span></a></li>
 		<li class="i_calendar_day"><a href="/?nav=calendar"><span>Calendar</span></a></li>
 		<li class="i_graph"><a href="/?nav=monitor"><span>Monitoring</span></a></li>
