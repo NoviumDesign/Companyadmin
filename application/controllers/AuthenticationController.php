4f63577bb5d713a461e7200a8d76ef5d121bfd71
Emil Blomquist <mr.emil.blomquist@gmail.com>
2013-03-08 00:30:01 +0100

    another fix

--- application/controllers/AuthenticationController.php
+++ application/controllers/AuthenticationController.php
@@ -37,13 +37,6 @@ class AuthenticationController extends Zend_Controller_Action
             }
         }
         $this->view->form = $form;
-
-        if($_SESSION['access_denied']) {
-            echo '<div style="display:none">';
-            print_r($_SESSION['access_denied']);
-            echo '</div>';
-        }
-
     }
 
     public function logoutAction()
